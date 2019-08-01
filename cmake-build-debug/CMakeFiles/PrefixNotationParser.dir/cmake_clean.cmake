b7a4efa45762b4be58e64dcad89ab766fca8929e
Shiro126 <241309@student.pwr.edu.pl>
2019-08-01 18:30:47 +0200

    Initializing project

---
+++ cmake-build-debug/CMakeFiles/PrefixNotationParser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrefixNotationParser.dir/main.cpp.o"
+  "PrefixNotationParser.pdb"
+  "PrefixNotationParser"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PrefixNotationParser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
